24d220417cfdda479c394e8c4ef8bf37e8387a67
Maximilian <maxmueller2003@web.de>
2022-02-13 19:38:59 +0100

    changed .gitignore again

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,29 @@
 public class Main{
+
+    int[] unsortedArray;
+    int[] sortedArray;
+
+    public Main(int arraySize) {
+        createUnsortedArray(arraySize);
+    }
+
+    private void createUnsortedArray(int length) {
+        unsortedArray = new int[length];
+    }
+
+    private void printUnsortedArray() {
+        System.out.println("Before:");
+        for (int i = 0; i < unsortedArray.length; i++) {
+            System.out.println(unsortedArray[i]);
+        }
+    }
+
+    private void printSortedArray() {
+        System.out.println("After:");
+        for (int i = 0; i < sortedArray.length; i++) {
+            System.out.println(sortedArray[i]);
+        }
+    }
     public static void main(String[] args) {
         
     }
